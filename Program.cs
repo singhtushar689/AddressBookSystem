31c9d82c420b08c978b17112087314e0b219183e
singhtushar689 <singhtushar689@gmail.com>
2022-12-27 17:02:49 +0530

    Creating Address Book

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AddressBookSystem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBoook");
+            //Person person1= new Person("name", "address", "city", "state", "zipCode", "phoneNum");
+            Console.WriteLine("Enter initial num of Address");
+            int n =Convert.ToInt32(Console.ReadLine());
+            Person.addList(n);
+            Person.printList();
+        }   
+           
+    }
+}
