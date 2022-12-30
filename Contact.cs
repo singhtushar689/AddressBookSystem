e7ecd0c17f84e5c8ceecb815e69d21d4784a1fea
singhtushar689 <singhtushar689@gmail.com>
2022-12-30 11:35:10 +0530

    Created AddressBook

---
+++ Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string MobileNumber { get; set; }
+        public string ZipCode { get; set; }
+        public string Email { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,14 +2,45 @@
 {
     public class Program
     {
+        private static string? FirstName;
+        private static string? LastName;
+        private static string? Address;
+        private static string? City;
+        private static string? MobileNumber;
+        private static string? State;
+        private static string? ZipCode;
+        private static string? Email;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBoook");
-            Console.WriteLine("Enter Number of Contacts you want to enter");
-            int n =Convert.ToInt32(Console.ReadLine());
-            Person.addList(n);
-            Person.addNewContact("name","address","city","state","zipCode","phoneNum");
-            Person.printList();
+            Console.WriteLine("Hello World");
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("Welcome to AddressBook");
+                Console.WriteLine("Choose options given below: /n 1. Create Contacts /n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the FirstName,Lastname,Address,City,State,Mobile,Email & zip");
+                        Contact contact = new Contact();
+                        {
+                            FirstName = Console.ReadLine();
+                            LastName = Console.ReadLine();
+                            Address = Console.ReadLine();
+                            City = Console.ReadLine();
+                            MobileNumber = Console.ReadLine();
+                            State = Console.ReadLine();
+                            ZipCode = Console.ReadLine();
+                            Email = Console.ReadLine();
+                        };
+                        break;
+                        case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }         
     }
 }
